f91d8f40d35a1fce9d0d531bbf2057eef61c1367
sgnm <suga315shin@gmail.com>
2015-11-27 00:09:17 +0900

    add GameMenu view

--- app/src/main/java/shimpeisuganuma/fuci/ConnectProcessing.java
+++ app/src/main/java/shimpeisuganuma/fuci/ConnectProcessing.java
@@ -1,6 +1,7 @@
 package shimpeisuganuma.fuci;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -11,26 +12,22 @@ import android.widget.ImageView;
 /**
  * Created by Shin on 2015/11/24.
  */
-public class ConnectProcessing extends Activity implements View.OnClickListener {
+public class ConnectProcessing extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.processing_connect);
 
-        ImageView imgPros = (ImageView)findViewById(R.id.Connecting);
-        imgPros.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view){
-        ImageView imgPros = (ImageView)findViewById(R.id.Connecting);
-        imgPros.setImageResource(R.drawable.connected);
-
         new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
             public void run() {
+                ImageView imgPros = (ImageView)findViewById(R.id.Connecting);
+                imgPros.setImageResource(R.drawable.connected);
                 Log.d("TAG", "2000ms delayed");
+                Intent intent = new Intent(ConnectProcessing.this, GameMenu.class);
+                startActivity(intent);
             }
         }, 2000);
     }
+
 }
---
+++ app/src/main/java/shimpeisuganuma/fuci/GameMenu.java
@@ -0,0 +1,15 @@
+package shimpeisuganuma.fuci;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Shin on 2015/11/26.
+ */
+public class GameMenu extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_game);
+    }
+}
