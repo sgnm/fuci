c7545d94e44b52c7c15b678db5eda0d98aa8d136
sgnm <suga315shin@gmail.com>
2015-11-26 23:21:20 +0900

    add ConnectProcessing view

--- app/src/main/java/shimpeisuganuma/fuci/ConnectProcessing.java
+++ app/src/main/java/shimpeisuganuma/fuci/ConnectProcessing.java
@@ -1,15 +1,36 @@
 package shimpeisuganuma.fuci;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 /**
  * Created by Shin on 2015/11/24.
  */
-public class ConnectProcessing extends ActionBarActivity {
+public class ConnectProcessing extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.processing_connect);
+
+        ImageView imgPros = (ImageView)findViewById(R.id.Connecting);
+        imgPros.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        ImageView imgPros = (ImageView)findViewById(R.id.Connecting);
+        imgPros.setImageResource(R.drawable.connected);
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Log.d("TAG", "2000ms delayed");
+            }
+        }, 2000);
     }
 }
--- app/src/main/java/shimpeisuganuma/fuci/MenuTop.java
+++ app/src/main/java/shimpeisuganuma/fuci/MenuTop.java
@@ -1,15 +1,27 @@
 package shimpeisuganuma.fuci;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 /**
  * Created by Shin on 2015/11/26.
  */
-public class MenuTop extends Activity {
+public class MenuTop extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.top_menu);
+
+        ImageView imgPlay = (ImageView)findViewById(R.id.MenuPlay);
+        imgPlay.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(MenuTop.this, ConnectProcessing.class);
+        startActivity(intent);
     }
 }
