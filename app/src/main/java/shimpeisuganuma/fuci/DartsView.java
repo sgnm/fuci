727d3d96a2df4d124ce766cb4f87b38507dccc99
sgnm <suga315shin@gmail.com>
2015-11-27 00:21:04 +0900

    add DartsView class

---
+++ app/src/main/java/shimpeisuganuma/fuci/DartsView.java
@@ -0,0 +1,15 @@
+package shimpeisuganuma.fuci;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Shin on 2015/11/27.
+ */
+public class DartsView extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.view_darts);
+    }
+}
--- app/src/main/java/shimpeisuganuma/fuci/GameMenu.java
+++ app/src/main/java/shimpeisuganuma/fuci/GameMenu.java
@@ -1,15 +1,27 @@
 package shimpeisuganuma.fuci;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 /**
  * Created by Shin on 2015/11/26.
  */
-public class GameMenu extends Activity {
+public class GameMenu extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_game);
+
+        ImageView imgDarts = (ImageView)findViewById(R.id.Darts);
+        imgDarts.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(this, DartsView.class);
+        startActivity(intent);
     }
 }
