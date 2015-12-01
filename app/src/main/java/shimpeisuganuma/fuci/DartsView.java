faa9b643d764f34ab1b92fdedba1567f872a5ad6
sgnm <suga315shin@gmail.com>
2015-12-02 00:37:31 +0900

    add func reset score

--- app/src/main/java/shimpeisuganuma/fuci/DartsView.java
+++ app/src/main/java/shimpeisuganuma/fuci/DartsView.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,6 +21,9 @@ public class DartsView extends Activity implements View.OnClickListener {
     int j = 0;
     int player = 0;
 
+    TextView strSum1; TextView strSum2;
+    TextView score1; TextView score2; TextView score3; TextView score4; TextView score5; TextView score6;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -27,11 +31,19 @@ public class DartsView extends Activity implements View.OnClickListener {
 
         Button btn = (Button)findViewById(R.id.btn);
         btn.setOnClickListener(this);
-        Button btnChange = (Button)findViewById(R.id.btnChange);
-        btnChange.setOnClickListener(this);
+        Button btnReset = (Button)findViewById(R.id.btnReset);
+        btnReset.setOnClickListener(this);
 
-        TextView strSum1 = (TextView)findViewById(R.id.sum1);
+        strSum1 = (TextView)findViewById(R.id.sum1);
         strSum1.setTextColor(Color.RED);
+
+        strSum2 = (TextView)findViewById(R.id.sum2);
+        score1 = (TextView) findViewById(R.id.score1);
+        score2 = (TextView) findViewById(R.id.score2);
+        score3 = (TextView) findViewById(R.id.score3);
+        score4 = (TextView) findViewById(R.id.score4);
+        score5 = (TextView) findViewById(R.id.score5);
+        score6 = (TextView) findViewById(R.id.score6);
     }
 
 
@@ -42,33 +54,26 @@ public class DartsView extends Activity implements View.OnClickListener {
                 setScore(player);
                 i++;
                 break;
-            case R.id.btnChange:
-                j++;
-                player = j % 2;
-                Log.d("player", String.valueOf(player));
-                dispPlayer(player);
+            case R.id.btnReset:
+                resetScore();
                 break;
         }
     }
 
     public void setScore(int player){
-        int n = (int) (Math.random() * 100) + 1;
-        Log.d("NUM", String.valueOf(n));
+        int n = (int) (Math.random() * 10) + 1;
         switch (player){
             case 0:
                 switch (i %= 3) {
                     case 0:
-                        TextView score1 = (TextView) findViewById(R.id.score1);
                         score1.setText(String.valueOf(n));
                         s1 = n;
                         break;
                     case 1:
-                        TextView score2 = (TextView) findViewById(R.id.score2);
                         score2.setText(String.valueOf(n));
                         s2 = n;
                         break;
                     case 2:
-                        TextView score3 = (TextView) findViewById(R.id.score3);
                         score3.setText(String.valueOf(n));
                         s3 = n;
                         calcScore(player);
@@ -78,17 +83,14 @@ public class DartsView extends Activity implements View.OnClickListener {
             case 1:
                 switch (i %= 3) {
                     case 0:
-                        TextView score4 = (TextView) findViewById(R.id.score4);
                         score4.setText(String.valueOf(n));
                         s4 = n;
                         break;
                     case 1:
-                        TextView score5 = (TextView) findViewById(R.id.score5);
                         score5.setText(String.valueOf(n));
                         s5 = n;
                         break;
                     case 2:
-                        TextView score6 = (TextView) findViewById(R.id.score6);
                         score6.setText(String.valueOf(n));
                         s6 = n;
                         calcScore(player);
@@ -103,23 +105,23 @@ public class DartsView extends Activity implements View.OnClickListener {
             case 0:
                 int oldSum = s1 + s2 + s3;
                 sum1 += oldSum;
-                TextView strSum1 = (TextView)findViewById(R.id.sum1);
                 strSum1.setText(String.valueOf(sum1));
-                Log.d("SUM", String.valueOf(sum1));
                 break;
             case 1:
                 int oldSum2 = s4 + s5 + s6;
                 sum2 += oldSum2;
-                TextView strSum2 = (TextView)findViewById(R.id.sum2);
                 strSum2.setText(String.valueOf(sum2));
-                Log.d("SUM", String.valueOf(sum2));
                 break;
         }
     }
 
+    public void resetScore(){
+        s1 = 0; s2 = 0; s3 = 0; s4 = 0; s5 = 0; s6 = 0; sum1 = 0; sum2 = 0;
+        score1.setText("0"); score2.setText("0"); score3.setText("0"); score4.setText("0"); score5.setText("0"); score6.setText("0");
+        strSum1.setText("0"); strSum2.setText("0");
+    }
+
     public void dispPlayer(int player){
-        TextView strSum1 = (TextView)findViewById(R.id.sum1);
-        TextView strSum2 = (TextView)findViewById(R.id.sum2);
         switch (player){
             case 0:
                 strSum1.setTextColor(Color.RED);
@@ -129,6 +131,25 @@ public class DartsView extends Activity implements View.OnClickListener {
                 strSum1.setTextColor(Color.WHITE);
                 strSum2.setTextColor(Color.RED);
                 break;
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float touchX = event.getX(), touchY = event.getY();
+        float r = 230;
+        float posX = 0;
+        float posY = 460;
+
+        double length = Math.hypot(touchX - posX, touchY - posY);
+
+        if (length <= r && event.getAction() == MotionEvent.ACTION_UP) {
+            j++;
+            player = j % 2;
+            dispPlayer(player);
+            return true;
+        } else {
+            return false;
         }
     }
 }
--- app/src/main/java/shimpeisuganuma/fuci/DrumsView.java
+++ app/src/main/java/shimpeisuganuma/fuci/DrumsView.java
@@ -2,14 +2,23 @@ package shimpeisuganuma.fuci;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 /**
  * Created by Shin on 2015/11/29.
  */
 public class DrumsView extends Activity {
+
+    Button hat; Button snare; Button clap; Button bass;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_drums);
+
+        hat = (Button) findViewById(R.id.hat);
+        snare = (Button) findViewById(R.id.snare);
+        clap = (Button) findViewById(R.id.clap);
+        bass = (Button) findViewById(R.id.bass);
     }
 }
