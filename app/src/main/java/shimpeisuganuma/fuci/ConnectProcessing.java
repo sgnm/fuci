4dc981db4a6034c2a64eb6aa5ac1c94d3a6d541f
sgnm <suga315shin@gmail.com>
2015-12-02 12:16:09 +0900

    add func bluetooth on off

--- app/src/main/java/shimpeisuganuma/fuci/ConnectProcessing.java
+++ app/src/main/java/shimpeisuganuma/fuci/ConnectProcessing.java
@@ -1,6 +1,7 @@
 package shimpeisuganuma.fuci;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -16,29 +17,23 @@ import android.widget.ImageView;
  * Created by Shin on 2015/11/24.
  */
 public class ConnectProcessing extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.processing_connect);
 
-        WindowManager windowManager = getWindowManager();
-        Display display = windowManager.getDefaultDisplay();
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        display.getMetrics(displayMetrics);
+//        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                ImageView imgPros = (ImageView)findViewById(R.id.Connecting);
+//                imgPros.setImageResource(R.drawable.connected);
+//                Log.d("TAG", "2000ms delayed");
+//                Intent intent = new Intent(ConnectProcessing.this, MenuTop.class);
+//                startActivity(intent);
+//            }
+//        }, 2000);
 
-        Log.v("width",          String.valueOf(display.getWidth()));       // 非推奨
-        Log.v("height",         String.valueOf(display.getHeight()));
 
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                ImageView imgPros = (ImageView)findViewById(R.id.Connecting);
-                imgPros.setImageResource(R.drawable.connected);
-                Log.d("TAG", "2000ms delayed");
-                Intent intent = new Intent(ConnectProcessing.this, MenuTop.class);
-                startActivity(intent);
-            }
-        }, 2000);
     }
-
 }
--- app/src/main/java/shimpeisuganuma/fuci/MainActivity.java
+++ app/src/main/java/shimpeisuganuma/fuci/MainActivity.java
@@ -1,31 +1,68 @@
 package shimpeisuganuma.fuci;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    private static final int REQUEST_ENABLE_BLUETOOTH = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView imageTop = (ImageView) findViewById(R.id.Top);
-        imageTop.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, MenuTop.class);
-                startActivity(intent);
-            }
-        });
+        //BluetoothAdapter取得
+        BluetoothAdapter Bt = BluetoothAdapter.getDefaultAdapter();
+        if(!Bt.equals(null)){
+            //Bluetooth対応端末の場合の処理
+            Log.d("BLE", "Bluetoothがサポートされてます。");
+        }else{
+            //Bluetooth非対応端末の場合の処理
+            Log.d("BLE", "Bluetoothがサポートれていません。");
+            finish();
+        }
+
+        boolean btEnable = Bt.isEnabled();
+        if(btEnable == true){
+            //BluetoothがONだった場合の処理
+            Log.d("BLE", "BLEはONです");
+            Intent intent = new Intent(MainActivity.this, ConnectProcessing.class);
+            startActivity(intent);
+        }else{
+            //OFFだった場合、ONにすることを促すダイアログを表示する画面に遷移
+            Log.d("BLE", "BLEはOFFです、ONにしてください");
+            Intent btOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(btOn, REQUEST_ENABLE_BLUETOOTH);
+        }
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int ResultCode, Intent date){
+        //ダイアログ画面から結果を受けた後の処理を記述
+        if(requestCode == REQUEST_ENABLE_BLUETOOTH){
+            if(ResultCode == Activity.RESULT_OK){
+                //BluetoothがONにされた場合の処理
+                Log.d("BLE", "BluetoothをONにしてもらえました。");
+                Intent intent = new Intent(MainActivity.this, ConnectProcessing.class);
+                startActivity(intent);
+            }else{
+                Log.d("BLE", "BluetoothがONにしてもらえませんでした。");
+                Toast.makeText(this, "BluetoothをONにして、再起動してください。", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
