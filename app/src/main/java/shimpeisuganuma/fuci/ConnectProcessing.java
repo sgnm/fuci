91c8b4fcf8b6d04e1cef5783594294657c48dfcf
sgnm <suga315shin@gmail.com>
2015-12-02 13:01:42 +0900

    add func get val using serial

--- app/src/main/java/shimpeisuganuma/fuci/ConnectProcessing.java
+++ app/src/main/java/shimpeisuganuma/fuci/ConnectProcessing.java
@@ -2,38 +2,189 @@ package shimpeisuganuma.fuci;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.os.Message;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Display;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.UUID;
 
 /**
  * Created by Shin on 2015/11/24.
  */
-public class ConnectProcessing extends Activity {
+public class ConnectProcessing extends Activity implements Runnable {
+
+    /* tag */
+    private static final String TAG = "BluetoothSample";
+
+    /* Bluetooth Adapter */
+    private BluetoothAdapter mAdapter;
+
+    /* Bluetoothデバイス */
+    private BluetoothDevice mDevice;
+
+    /* Bluetooth UUID(固定) */
+    private final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    /* デバイス名 環境に合わせて変更*/
+    private final String DEVICE_NAME = "HC-06";
+
+    /* Soket */
+    private BluetoothSocket mSocket;
+
+    /* Thread */
+    private Thread mThread;
+
+    /* Threadの状態を表す */
+    private boolean isRunning;
+
+    /** Action(ステータス表示). */
+    private static final int VIEW_STATUS = 0;
+
+    /** Action(取得文字列). */
+    private static final int VIEW_INPUT = 1;
+
+    /** Connect確認用フラグ */
+    private boolean connectFlg = false;
+
+    /** BluetoothのOutputStream. */
+    OutputStream mmOutputStream = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.processing_connect);
 
-//        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                ImageView imgPros = (ImageView)findViewById(R.id.Connecting);
-//                imgPros.setImageResource(R.drawable.connected);
-//                Log.d("TAG", "2000ms delayed");
-//                Intent intent = new Intent(ConnectProcessing.this, MenuTop.class);
-//                startActivity(intent);
-//            }
-//        }, 2000);
+        // Bluetoothのデバイス名を取得
+        // デバイス名は、RNBT-XXXXになるため、
+        // DVICE_NAMEでデバイス名を定義
+        mAdapter = BluetoothAdapter.getDefaultAdapter();
+//        mStatusTextView.setText("SearchDevice");
+        Set< BluetoothDevice > devices = mAdapter.getBondedDevices();
+        for ( BluetoothDevice device : devices){
 
+            if(device.getName().equals(DEVICE_NAME)){
+//                mStatusTextView.setText("find: " + device.getName());
+                Log.d("BLE", "find: " + device.getName());
+                mDevice = device;
+            }
+        }
 
+        mThread = new Thread(this);
+        // Threadを起動し、Bluetooth接続
+        isRunning = true;
+        mThread.start();
     }
+
+    // 別のアクティビティが起動した場合の処理
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        isRunning = false;
+        connectFlg = false;
+
+        try{
+            mSocket.close();
+        }
+        catch(Exception e){}
+    }
+
+    // スレッド処理(connectボタン押下後に実行)
+    @Override
+    public void run() {
+        InputStream mmInStream = null;
+
+        Message valueMsg = new Message();
+        valueMsg.what = VIEW_STATUS;
+        valueMsg.obj = "connecting...";
+        mHandler.sendMessage(valueMsg);
+
+        try{
+
+            // 取得したデバイス名を使ってBluetoothでSocket接続
+            mSocket = mDevice.createRfcommSocketToServiceRecord(MY_UUID);
+            mSocket.connect();
+            mmInStream = mSocket.getInputStream();
+            mmOutputStream = mSocket.getOutputStream();
+
+            // InputStreamのバッファを格納
+            byte[] buffer = new byte[1024];
+
+            // 取得したバッファのサイズを格納
+            int bytes;
+            valueMsg = new Message();
+            valueMsg.what = VIEW_STATUS;
+            valueMsg.obj = "connected.";
+            mHandler.sendMessage(valueMsg);
+
+            connectFlg = true;
+
+            while(isRunning){
+
+                // InputStreamの読み込み
+                bytes = mmInStream.read(buffer);
+                Log.i(TAG,"bytes="+bytes);
+                // String型に変換
+                String readMsg = new String(buffer, 0, bytes);
+
+                // null以外なら表示
+                if(readMsg.trim() != null && !readMsg.trim().equals("")){
+                    Log.i(TAG,"value="+readMsg.trim());
+
+                    valueMsg = new Message();
+                    valueMsg.what = VIEW_INPUT;
+                    valueMsg.obj = readMsg;
+                    mHandler.sendMessage(valueMsg);
+                }
+            }
+        }
+        // エラー処理
+        catch(Exception e){
+
+            valueMsg = new Message();
+            valueMsg.what = VIEW_STATUS;
+            valueMsg.obj = "Error1:" + e;
+            mHandler.sendMessage(valueMsg);
+
+            try{
+                mSocket.close();
+            }catch(Exception ee){}
+            isRunning = false;
+            connectFlg = false;
+        }
+    }
+
+    /**
+     * 描画処理はHandlerでおこなう
+     */
+    Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            int action = msg.what;
+            String msgStr = (String)msg.obj;
+            if(action == VIEW_INPUT){
+//                mInputTextView.setText(msgStr);
+                Log.d("BLE", "input value: " + msgStr);
+            }
+            else if(action == VIEW_STATUS){
+//                mStatusTextView.setText(msgStr);
+                Log.d("BLE", "status: " + msgStr);
+            }
+        }
+    };
 }
